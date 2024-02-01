c0f30d68d7284b3216c076476916880e8f97f607
tinnapop <tinnapos@scg.com>
2024-02-01 23:53:17 +0700

    refactor for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      }
-    ]
-  }
+  output: 'export'
 }
 
 export default nextConfig
