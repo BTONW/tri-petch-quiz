4c9dd25a180e3884d72811a00cd97c0e89ccf61b
tinnapop <tinnapos@scg.com>
2024-02-02 08:09:05 +0700

    fix next js assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  assetPrefix: 'https://btonw.github.io/tri-petch-quiz/examples'
+  
 }
 
 export default nextConfig
