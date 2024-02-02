52eb834fe0485215676bbae73701460c808a5912
tinnapop <tinnapos@scg.com>
2024-02-02 10:36:10 +0700

    fix invalid distDir

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   output: 'export',
-  distDir: 'ex-examples',
+  distDir: 'examples',
   assetPrefix: process.env.NEXT_PUBLIC_PREFIX_URL,
   env: {
     prefix_url: process.env.NEXT_PUBLIC_PREFIX_URL || ''
